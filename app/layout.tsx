e5693050fafb1c02f1d4a25d35d7ce32335d6a6a
sora-00 <sora.umiushi@gmail.com>
2024-08-01 16:52:19 +0900

    ファイル移動によるimportの更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Header from "./components/header";
+import Header from "./components/home/common/header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
